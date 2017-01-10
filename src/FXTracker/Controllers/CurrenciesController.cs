f34a0d804b0c873e0f9b872ee717c005e142640d
forattila <forattila@gmail.com>
2017-01-10 13:06:13 +0100

    rates vq

--- src/FXTracker/Controllers/CurrenciesController.cs
+++ src/FXTracker/Controllers/CurrenciesController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using FXTracker.Model;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Diagnostics;
+using System.Xml.Linq;
+using System.Globalization;
 
 namespace FXTracker.Controllers
 {
@@ -24,6 +28,43 @@ namespace FXTracker.Controllers
             return result;
         }
 
+        [Route(nameof(FXRates))]
+        [HttpGet]
+        public IEnumerable<FXRate> FXRates()
+        {
+            List<FXRate> result = new List<FXRate>();
+
+            HttpClient httpClient = new HttpClient();
+            var httpResult = httpClient.GetAsync("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml").Result;
+            var responseString = httpResult.Content.ReadAsStringAsync().Result;
+            XDocument doc = XDocument.Parse(responseString);
+            var items = doc.Descendants().Where(d => d.Name.LocalName == "Cube" && d.Attribute("time")!=null);
+            if (items != null) {
+                items.ToList().ForEach(item =>
+                {
+                var subItems = item.Elements();
+                if (subItems != null)
+                {
+                    subItems.ToList().ForEach(subItem =>{
+
+                        DateTime date;
+                        DateTime.TryParse(item.Attribute("time").Value, out date);
+                        double rate;
+                        Double.TryParse(subItem.Attribute("rate").Value, System.Globalization.NumberStyles.Any, CultureInfo.InvariantCulture, out rate);
+                        string currency = subItem.Attribute("currency").Value;
+
+                        if (date != default(DateTime) && rate != default(double) && !String.IsNullOrEmpty(currency))
+                        {
+                            result.Add(new FXRate("EUR/" + currency, date, rate));
+                        }
+                    });
+                    }
+    
+                });
+            }
+            
+            return result;
+        }
  
     }
 }
---
+++ src/FXTracker/Model/FXRate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FXTracker.Model
+{
+    public class FXRate
+    {
+        public string Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public double Rate { get; set; }
+
+        public FXRate(string id, DateTime date, double rate)
+        {
+            Id = id;
+            Date = date;
+            Rate = rate;
+        }
+
+    }
+}
